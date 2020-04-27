64320dda976b30c998b2cff82a57cb14f809cc62
oranium <a.gaden@campus.fct.unl.pt>
2020-04-27 14:24:04 +0200

    update glue functions and fix frequency and F

--- localmaxs.py
+++ localmaxs.py
@@ -4,32 +4,73 @@ import nltk
 signs_end = {';' : ' ', ':': ' ', '!': ' ', '?': ' ', '<': ' ', '>': ' ', '&': ' ', ')': ' ', ']': ' ', '[': ' ', '.': ' ', '"':' '}
 signs_beginning = {'(': ' ', '<': ' ', '>': ' ', '"':' ' }
 
+# the frequency of the ngram in our corpus
 def freq(ngram):
-     return words.count(ngram)
+    # so the letters of 1-grams dont get counted individually
+    ngram = ngram if type(ngram) is list else [ngram]
+    n = len(ngram)
+    print(ngram)
+    _freq = sum(1 for i in range(len(words)) if words[i:i+n] == ngram )
+    print(_freq)
+    return sum(1 for i in range(len(words)) if words[i:i+n] == ngram )
 
+# the probability of the ngram in our corpus
 def prob(ngram):
+    ngram = ngram if type(ngram) is list else [ngram]
     return freq(ngram) * len(ngram) / len(words)
 
-def F1(ngram, i):
-    if i == len(ngram)-1:
-        return freq(ngram[0]) + freq(ngram[])
-    return F1(ngram[:i+1], i+1) + freq(ngram[i+1:])
 
+def F_rec(ngram, i=1):
+    # shouldnt get larger than length
+    if not i >= len(ngram):
+        return prob(ngram[:i]) * prob(ngram[i:])
+    return F_rec(ngram, i+1) + prob(ngram[:i]) * prob(ngram[i:])
+
+# F factor for ngrams n>2 from the slides
 def F(ngram):
-    return 1/(len(ngram)-1) * np.sum(F1(ngram, 0))
+    probs = 0
+    for i in range(1,len(ngram)):
+        probs += prob(ngram[:i]) * prob(ngram[i:]) 
+    return 1/(len(ngram)-1) * probs
 
-def glue(ngram,t='dice'):
+
+def dice(ngram):
+    if len(ngram) == 2:
+        return 2 * freq(ngram) / (freq(ngram[0]) + freq(ngram[1]))
+    return 2 * freq(ngram) / F(ngram)
+
+
+def scp(ngram):
+    if len(ngram) == 2:
+        # i imagine the joint probability of those ngrams is if they happen after another????
+        # slices so we concatenate the lists and not the strings
+        # otherwise we can just substitute it with ngram
+        return (prob(ngram[:1] + ngram[1:]) + prob(ngram[1] + ngram[0])) ** 2 / prob(ngram[0]) ** prob(ngram[1])
+    return prob(ngram) ** 2 / F_rec(ngram)
+
+def phi_squared(ngram):
+    if len(ngram) == 2:
+        # slices so we concatenate the lists and not the strings
+        # same as in scp
+        dividend = (len(words) * (freq(ngram[:1] + ngram[1:]) + freq(ngram[1] + ngram[0])) - freq(ngram[0]) + freq(ngram[1])) ** 2 
+        divisor = freq(ngram[0]) * freq(ngram[1]) * (len(words) - freq(ngram[0])) * (len(words) - freq(ngram[1]))
+        return dividend / divisor
+    
+    #avq = 
+    
+
+def glue(ngram, glue_func):
     '''
-    type scp, dice, theta2
+    type scp, dice, phi_squared
+    glue_func is the function to be passed, not a string or something
     '''
-    if g=='dice':
-        return (2 * freq(ngram)) / F(ngram)
+    return glue_func(ngram)
 
 
 def init_data(which='one'):
     with open('corpus2mw/fil_1', 'r') as txt:
         words = txt.read()
-        words = words.replace('\n')
+        words = words.replace('\n', '')
         for k,v in signs_end.items():
             words = words.replace(k, v+k)
         for k,v in signs_beginning.items():
@@ -37,11 +78,11 @@ def init_data(which='one'):
         words = words.split(' ')
         return words
 
-words [['a','b','c']
-''' words = init_data()
+words = init_data()
+# i think this is useless
 words_ngrams = [words, [words[i:i+2] for i in range(len(words))],
                 [words[i:i+3] for i in range(len(words))],[words[i:i+4] for i in range(len(words))],
                 [words[i:i+5] for i in range(len(words))],[words[i:i+6] for i in range(len(words))],
                 [words[i:i+7] for i in range(len(words))]]
-print(words_ngrams)
-'''
+
+print(glue(words_ngrams[1][2], dice))
