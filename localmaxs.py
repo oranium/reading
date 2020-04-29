d925ab0f91066d3481ca1527471c52a9045595f1
oranium <a.gaden@campus.fct.unl.pt>
2020-04-29 15:35:46 +0200

    fix about 400 mistakes, phi is weird

--- localmaxs.py
+++ localmaxs.py
@@ -9,9 +9,9 @@ def freq(ngram):
     # so the letters of 1-grams dont get counted individually
     ngram = ngram if type(ngram) is list else [ngram]
     n = len(ngram)
-    print("ngram:",ngram)
+    #print("ngram:",ngram)
     _freq = sum(1 for i in range(N) if words[i:i+n] == ngram )
-    print("freq",_freq)
+    #print("freq",_freq)
     return _freq
 
 # the probability of the ngram in our corpus
@@ -21,32 +21,42 @@ def prob(ngram):
 
 
 # Needs to be averaged by the caller! (divide by 1/len_ngram -1)
-def F_rec(ngram, i=1):
+def F_rec_dice(ngram, i=1):
     # shouldnt get larger than length
-    if not i >= len(ngram):
-        return prob(ngram[:i]) * prob(ngram[i:])
-    return prob(ngram[:i]) * prob(ngram[i:]) + F_rec(ngram, i+1)
+    #print(f'F_rec called for {ngram[:i]}, {ngram[i:]}')
+    if i >= len(ngram)-1:
+        #print('calling freq')
+        return freq(ngram[:i]) + freq(ngram[i:])
+    return freq(ngram[:i]) + freq(ngram[i:]) + F_rec_dice(ngram, i+1)
+
+# Needs to be averaged by the caller! (divide by 1/len_ngram -1)
+def F_rec_scp(ngram, i=1):
+    # shouldnt get larger than length
+    if i >= len(ngram)-1:
+        return prob(ngram[:i]) + prob(ngram[i:])
+    return prob(ngram[:i]) + prob(ngram[i:]) + F_rec_scp(ngram, i+1)
 
 # F factor for ngrams n>2 from the slides
-def F(ngram):
-    probs = 0
+def F_dice(ngram):
+    freqs = 0
     for i in range(1,len(ngram)):
-        probs += prob(ngram[:i]) * prob(ngram[i:]) 
-    return 1/(len(ngram)-1) * probs
+        print(f'F called for {ngram[:i]}, {ngram[i:]}')
+        freqs += freq(ngram[:i]) * freq(ngram[i:]) 
+    return 1/(len(ngram)-1) * freqs
 
 
 # Needs to be averaged by the caller! (divide by 1/len_ngram -1)
 def _avq(ngram, i=1):
     # shouldn't get larger than length
-    if i >= len(ngram):
-        return 1/(i-1) * freq(ngram[:i]) * freq(ngram[i:])
+    if i >= len(ngram) - 1:
+        return freq(ngram[:i]) * freq(ngram[i:])
     return freq(ngram[:i]) * freq(ngram[i:]) * _avq(ngram, i+1)
 
 
 # Needs to be averaged by the caller! (divide by 1/len_ngram -1)
 def _avd(ngram, i=1):
     # shouldn't get larger than length
-    if i >= len(ngram):
+    if i >= len(ngram) - 1:
         return freq(ngram[:i]) * freq(ngram[i:]) * (N - freq(ngram[:i])) * (N - freq(ngram[i:]))
     return freq(ngram[:i]) * freq(ngram[i:]) * (N - freq(ngram[:i])) * (N - freq(ngram[i:])) + _avd(ngram, i+1)
 
@@ -54,29 +64,27 @@ def _avd(ngram, i=1):
 def dice(ngram):
     if len(ngram) == 2:
         return 2 * freq(ngram) / (freq(ngram[0]) + freq(ngram[1]))
-    return 2 * freq(ngram) / F(ngram)
+    F_dice = (1 / (len(ngram) - 1)) * F_rec_dice(ngram)
+    return 2 * freq(ngram) / F_dice
 
 
 def scp(ngram):
     if len(ngram) == 2:
-        # i imagine the joint probability of those ngrams is if they happen after another????
-        # slices so we concatenate the lists and not the strings
-        # otherwise we can just substitute it with ngram
-        return (prob(ngram[:1] + ngram[1:]) + prob(ngram[1] + ngram[0])) ** 2 / prob(ngram[0]) ** prob(ngram[1])
-    return prob(ngram) ** 2 / (1 / (len(ngram) - 1) * F_rec(ngram))
+        return prob(ngram) ** 2 / prob(ngram[0]) * prob(ngram[1])
+    F_scp = (1 / (len(ngram) - 1) * F_rec_scp(ngram))
+    return prob(ngram) ** 2 / F_scp
 
 def phi_squared(ngram):
     if len(ngram) == 2:
-        # slices so we concatenate the lists and not the strings
-        # same as in scp
-        dividend = (N * (freq(ngram[:1] + ngram[1:]) + freq(ngram[1] + ngram[0])) - freq(ngram[0]) + freq(ngram[1])) ** 2 
+        # not sure if there is a precedence error in slides
+        dividend = (N * freq(ngram) - (freq(ngram[0]) * freq(ngram[1])) ) ** 2 
         divisor = freq(ngram[0]) * freq(ngram[1]) * (N - freq(ngram[0])) * (N - freq(ngram[1]))
         return dividend / divisor
     
     avq = 1 / (len(ngram)-1) * _avq(ngram) 
     avd = 1 / (len(ngram)-1) * _avd(ngram)
 
-    return (N * freq(ngram) - avq) ** 2 / avd
+    return ((N * freq(ngram) - avq) ** 2) / avd
 
 def glue(ngram, glue_func):
     '''
@@ -109,8 +117,18 @@ words_ngrams = [words, [words[i:i+2] for i in range(N)],
 #     for j in range (3 ):
 #         print("Dice",glue(words_ngrams[i][j], dice))
 #         print(words_ngrams[i][j])
+# we will need a lot of glue for this!
+for n in range(3,8):
+    print(f'starting {n}-grams')
+    for word in words:
+        idx = words.index(word)
+        glue_scp = glue(words[idx:idx+n], scp) 
+        glue_dice = glue(words[idx:idx+n], dice) 
+        glue_phi = glue(words[idx:idx+n], phi_squared)
 
-print("Dice",glue(words_ngrams[2][0], dice))
-print("scp",glue(words_ngrams[3][0], scp))
-#print("phi_squared",glue(words_ngrams[3][0], phi_squared))
-print(words_ngrams[3][0])
+        if glue_scp > 1 or glue_dice > 1 or (0>glue_phi>1 and not glue_scp == 1):
+            print('That was a mistake') 
+            print(f'{words[idx:idx+n]}') 
+            print(glue_dice, glue_scp, glue_phi) 
+            raise EnvironmentError('Save the climate, save the world')
+print('yay')
